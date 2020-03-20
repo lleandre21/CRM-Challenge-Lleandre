e2238c75c931a889b8da69d5292849466e85ba41
lleandre21 <llleandre21@gmail.com>
2020-03-20 17:24:15 -0500

    added view page and routes for alphatized and missing email.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # get 'customers/index'
-  # get 'customers/show'
+  get 'customers/alphabetized', to: 'customers#alphabetized', as: '/customers/alphabetized'
+  get 'customers/missing_email', to: 'customers#missing_email', as: '/customers/missing_email'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources 'customers', only: %i[index show]
+  # resources 'customers', only: %i[index show alphabetized missing_email]
 
+  # Get /
   root to: 'customers#index'
 end
