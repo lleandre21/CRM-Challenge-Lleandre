2af9fd772bd00d7496fd117a4b785c09446079ca
lleandre21 <llleandre21@gmail.com>
2020-03-20 15:35:36 -0500

    added index action/view for customer controller.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,12 @@
+# frozen_string_literal: true
+
 class CustomersController < ApplicationController
   def index
+    @customer = Customer.all
+    # @customer = Customer.order(:name)
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 end
