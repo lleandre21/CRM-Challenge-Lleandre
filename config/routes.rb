ee2a830891b4f5904ca5d416ddbee599cb63fbfc
lleandre21 <llleandre21@gmail.com>
2020-03-20 14:58:35 -0500

    modified routes for show and index in customer view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  # get 'customers/index'
+  # get 'customers/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources 'customers', only: %i[index show]
+
+  root to: 'customers#index'
 end
