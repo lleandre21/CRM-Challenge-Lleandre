e1d14d33d8adbeb1352883c07a1d57a4e49b4646
lleandre21 <llleandre21@gmail.com>
2020-03-20 14:47:10 -0500

    Added image placeholder for customer model.

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Customer do
-  permit_params :fullname, :phone, :email, :notes
+  permit_params :fullname, :phone, :email, :notes, :image
+
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Customer < ApplicationRecord
+  validates :fullname, presence: true
+
+  has_one_attached :image
 end
